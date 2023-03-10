718a0dfa7346d0856a7872adc4565cf4f99623eb
RobinBosma <170027@student.horizoncollege.nl>
2023-03-10 09:49:35 +0100

    Navbar
    Added navbar in Index.php, added style.css file, navbar.php and footer.php. added a train for the footer and added 3 logos

---
+++ include/navbar.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Code Express</title>
+    <link rel="icon" href="../img/CodeExpress-logo.png" type="image/x-icon" />
+</head>
+<body>
+<nav>
+    <ul>
+        <li><img src="../img/logo.png" alt="Logo"> Code Express</li>
+        <li>
+            <a href="#">Code</a>
+            <ul>
+                <li><a href="#">HTML</a></li>
+                <li><a href="#">CSS</a></li>
+                <li><a href="#">JavaScript</a></li>
+            </ul>
+        </li>
+    </ul>
+      <form>
+        <input type="text" placeholder="What are you looking for?">
+        <input type="submit" value="Search">
+      </form>
+</nav>
+</body>
+</html>
---
+++ page/index.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <style>
+        <?php include '../style.css'; ?>
+    </style>
+</head>
+
+<body>
+        <!-- Navbar -->
+        <?php include "../include/navbar.php" ?>
+
+        <!-- Main Content -->
+
+        <!-- Footer -->
+        <?php include "../include/footer.php" ?>
+</body>
+
+</html>
