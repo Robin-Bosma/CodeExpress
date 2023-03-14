f1129c02d4adf1b27639e1546fb4bfce134f7d15
139388-Joris-Taam <116798965+139388-Joris-Taam@users.noreply.github.com>
2023-03-14 11:30:33 +0100

    insert code to database
    you can post the code to mysql

--- test.sql
+++ test.sql
@@ -1,7 +1,9 @@
 CREATE TABLE configuration (
-  id INT NOT NULL PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  code VARCHAR(255) NOT NULL,
   title VARCHAR(255) NOT NULL,
   category VARCHAR(255),
   date DATE,
   description TEXT
 );
+
