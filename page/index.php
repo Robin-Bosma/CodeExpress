74966b6f93b127a3e96dc8fd71cb8bd19ed51b84
139388-Joris-Taam <116798965+139388-Joris-Taam@users.noreply.github.com>
2023-03-14 09:08:41 +0100

    index content
    content index input fields

--- page/index.php
+++ page/index.php
@@ -11,13 +11,44 @@ session_start();
 </head>
 
 <body>
-        <!-- Navbar -->
-        <?php include "../include/navbar.php" ?>
+    <!-- Navbar -->
+    <?php include "../include/navbar.php" ?>
+    <!-- Main Content -->
+    <div class="index-container">
+        <div id="pasting-code">
+            <h1> Code:</h1>
+            <input type="text" value="">
+        </div>
+        <!-- configuration -->
+        <div id="container-row">
+            <div id="configuration">
+                <h1>Configuration</h1>
+                <div id="config-row">
+                    <div id="confing-title">
+                        <h2 id="title-mr">Title:</h2>
+                        <h2 id="category-mr">Category:</h2>
+                        <h2 id="date-mr">Date:</h2>
+                    </div>
+                    <div id="config-input">
+                        <input name="title" type="text" name="title">
+                        <input name="category" type="text" name="category">
+                        <input name="date" type="date" name="date">
+                    </div>
+                </div>
+            </div>
 
-        <!-- Main Content -->
-
-        <!-- Footer -->
-        <?php include "../include/footer.php" ?>
+            <!-- description -->
+            <div id="description">
+                <h1> description </h1>
+                <input id="description-input"  name="description" type="text" value="">
+            </div>
+        </div>
+    </div>
+    <!-- buttons post -->
+    <input type="submit" name="submit-public" value="Create public post">
+    <input type="submit" name="submit-privite" value="Create privite post">
+    <!-- Footer -->
+    <?php include "../include/footer.php"?>
 </body>
 
 </html>
