e7e9b3d99a379ef7d1a5812ce2a647987b442f39
Michel-3 <169541@student.horizoncollege.nl>
2023-03-14 10:52:59 +0100

    New file, navbar edited, code.php edited
    - The navbar Code button now links to the overview page
    - The problem_description element works
    - New php page, with navbar, footer, style and basic html form

---
+++ page/overview.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="icon" href="../img/CodeExpress-logo.png" type="image/x-icon" />
+    <style>
+        <?php include '../style.css'; ?>
+    </style>
+    <title>Overview - Code Express</title>
+</head>
+
+<body>
+
+    <!-- Navbar -->
+    <?php include "../include/navbar.php" ?>
+
+    <!-- Main Content -->
+    <h1>Dit is overview</h1>
+
+    <!-- Footer -->
+    <?php include "../include/footer.php" ?>
+
+</body>
+
+</html>
