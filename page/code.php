3b7f3672fd15766cc23a070dbbe3d1e58a83253f
Michel-3 <169541@student.horizoncollege.nl>
2023-03-15 09:18:39 +0100

    Merge conflict
    Fixed

--- page/code.php
+++ page/code.php
@@ -35,19 +35,35 @@ if (isset($_GET['id'])) {
     <?php include "../include/navbar.php" ?>
 
     <!-- Main Content -->
-    <h1><?php echo $title ?></h1>
-    <div class="c-row">
-        <h1>PHP</h1>
-        <h1><?php echo 'date created' ?></h1>
-        <h1><?php echo 'date edited' ?></h1>
-        <button id="copy-btn" onclick="copyToClipboard()">Copy Code</button>
-    </div>
-    <h1>Code:</h1>
-    <div id="code"></div>
+    <div class="code-container">
+        <div class="left-container">
+            <div class="title-box">
+                <h1><?php echo $title ?></h1>
+            </div>
+            <div>
+                <h1>PHP</h1>
+                <h1><?php echo 'date created' ?></h1>
+                <h1><?php echo 'date edited' ?></h1>
+                <button id="copy-btn" onclick="copyToClipboard()">Copy Code</button>
+            </div>
+            <h1>Code:</h1>
+            <div id="code"></div>
 
-    <h1>Comments</h1>
-    <input type="text" placeholder="Write your comment here">
-    <input type="submit" value="Add Comment">
+            <h1>Comments</h1>
+            <input type="text" placeholder="Write your comment here">
+            <input type="submit" value="Add Comment">
+        </div>
+        <div class="right-container">
+            <h1>Post History</h1>
+            <div class="right-container-content">
+                <p>PHP code for a Topo exam</p>
+                <div class="flex-direction-row">
+                    <p>PHP</p>
+                    <p>Date created</p>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <!-- Footer -->
     <?php include "../include/footer.php" ?>
@@ -55,6 +71,7 @@ if (isset($_GET['id'])) {
 </body>
 
 <script>
+    // The function for copying code
     function copyToClipboard() {
         var codeDiv = document.getElementById("code");
         var codeText = codeDiv.innerHTML;
