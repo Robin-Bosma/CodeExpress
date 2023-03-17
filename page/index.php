753aca756abe208a9250cdb2dde29ab02b2d3806
RobinBosma <170027@student.horizoncollege.nl>
2023-03-17 08:31:04 +0100

    index/ style
    added a style that didnt work so deleted it and made a change for some words

--- page/index.php
+++ page/index.php
@@ -23,7 +23,6 @@ include "../include/insert-post.php";
             <form method="post" action="../page/index.php">
                 <h1 class="config-code">Code:</h1>
                 <textarea cols="80" rows="10" id="code" type="text" name="code" required></textarea>
-
                 <h1 class="config">Configurations</h1>
         </div>
         <div class="item2">
@@ -40,22 +39,22 @@ include "../include/insert-post.php";
                 <p class="config-category">Category:</p>
                 <div id="checkboxes">
                     <div class="checkbox-top">
-                    <input type="checkbox" id="checkbox-codes" name="category[]" value="HTML">
-                    <label for="HTML">HTML</label><br>
-                    <input type="checkbox" id="checkbox-codes" name="category[]" value="CSS">
-                    <label for="CSS">CSS</label><br>
+                        <input type="checkbox" id="checkbox-codes" name="category[]" value="HTML">
+                        <label for="HTML">HTML</label><br>
+                        <input type="checkbox" id="checkbox-codes" name="category[]" value="CSS">
+                        <label for="CSS">CSS</label><br>
                     </div>
                     <div class="checkbox-center">
-                    <input type="checkbox" id="checkbox-codes" name="category[]" value="PHP">
-                    <label for="PHP">PHP</label><br>
-                    <input type="checkbox" id="checkbox-codes" name="category[]" value="JavaScript">
-                    <label for="JavaScript">JavaScript</label><br>
-                </div>
-                <div class="checkbox-bottom">
-                    <input type="checkbox" id="checkbox-codes" name="category[]" value="SQL">
-                    <label for="SQL">SQL</label><br>
+                        <input type="checkbox" id="checkbox-codes" name="category[]" value="PHP">
+                        <label for="PHP">PHP</label><br>
+                        <input type="checkbox" id="checkbox-codes" name="category[]" value="JavaScript">
+                        <label for="JavaScript">JavaScript</label><br>
                     </div>
-                
+                    <div class="checkbox-bottom">
+                        <input type="checkbox" id="checkbox-codes" name="category[]" value="SQL">
+                        <label for="SQL">SQL</label><br>
+                    </div>
+
                 </div>
             </div>
         </div>
@@ -65,58 +64,58 @@ include "../include/insert-post.php";
 </textarea>
         </div>
         <div class="item4">
-        <div class="right-container">
-            <h1>Post History</h1>
-            <div class="right-container-content">
-                <div class="line">
-                    <div class="flex-direction-row-height">
-                        <p class="margin-righter">Title</p>
-                        <p>Category</p>
+            <div class="right-container">
+                <h1>Post History</h1>
+                <div class="right-container-content">
+                    <div class="line">
+                        <div class="flex-direction-row-height">
+                            <p class="margin-righter">Title</p>
+                            <p>Category</p>
+                        </div>
                     </div>
-                </div>
-                <table>
-                    <?php
-                    // Retrieve data from the configuration table
-                    $sql = "SELECT * FROM configuration";
-                    $result = $pdo->query($sql);
+                    <table>
+                        <?php
+                        // Retrieve data from the configuration table
+                        $sql = "SELECT * FROM configuration";
+                        $result = $pdo->query($sql);
 
-                    if ($result->rowCount() > 0) {
-                        while ($row = $result->fetch()) {
-                            echo "<tr>";
-                            echo "<td><a class='margin-right' href='../page/code.php?id=" . $row['id'] . "'>" . $row["title"] . "</a></td>";
-                            echo "<td>" . $row["category"] . "</td>";
-                            echo "</tr>";
+                        if ($result->rowCount() > 0) {
+                            while ($row = $result->fetch()) {
+                                echo "<tr>";
+                                echo "<td><a class='margin-right' href='../page/code.php?id=" . $row['id'] . "'>" . $row["title"] . "</a></td>";
+                                echo "<td>" . $row["category"] . "</td>";
+                                echo "</tr>";
+                            }
+                        } else {
+                            echo "<tr><td colspan='2'>No results found</td></tr>";
                         }
-                    } else {
-                        echo "<tr><td colspan='2'>No results found</td></tr>";
-                    }
-                    ?>
-                </table>
-        </div>
-        </div>
+                        ?>
+                    </table>
                 </div>
-        <div class="item5">
-        <div><input type="submit" id="submit-public" href="code.php" value="Public post"> <input type="submit" id="submit-private" value="Private post"></div>
+            </div>
         </div>
-    </form>
+        <div class="item5">
+            <div><input type="submit" id="submit-public" href="code.php" value="Public post"> <input type="submit" id="submit-private" value="Private post"></div>
+        </div>
+        </form>
         <tbody>
             <div class="item6">
-            <?php
-            // Retrieve data from the configuration table
-            $sql = "SELECT * FROM configuration";
-            $result = $pdo->query($sql);
+                <?php
+                // Retrieve data from the configuration table
+                $sql = "SELECT * FROM configuration";
+                $result = $pdo->query($sql);
 
-            if ($result->rowCount() > 0) {
-                while ($row = $result->fetch()) {
-                    echo "<tr>";
-                    echo "<td>" . $row["title"] . "</td>";
-                    echo "<td>" . $row["category"] . "</td>";
-                    echo "</tr>";
+                if ($result->rowCount() > 0) {
+                    while ($row = $result->fetch()) {
+                        echo "<tr>";
+                        echo "<td>" . $row["title"] . "</td>";
+                        echo "<td>" . $row["category"] . "</td>";
+                        echo "</tr>";
+                    }
+                } else {
+                    echo "<tr><td colspan='2'>No results found</td></tr>";
                 }
-            } else {
-                echo "<tr><td colspan='2'>No results found</td></tr>";
-            }
-            ?>
+                ?>
             </div>
         </tbody>
         </table>
