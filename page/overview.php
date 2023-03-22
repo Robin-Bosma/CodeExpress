bafe01d69a965619317bc4112ae9ee2f4a63cfe1
139388-Joris-Taam <116798965+139388-Joris-Taam@users.noreply.github.com>
2023-03-22 08:58:29 +0100

    overview
    overview styling

--- page/overview.php
+++ page/overview.php
@@ -22,23 +22,35 @@ $stmt = $pdo->prepare("SELECT * FROM configuration");
 $stmt->execute();
 $code_array = $stmt->fetchAll(PDO::FETCH_OBJ);
 
-function echoCode($code_array) {
+function echoCode($code_array)
+{
     foreach ($code_array as $key) {
-        echo $key->title;
-        echo "<a href='code.php?id=$key->id'>Details</a>";
+        echo "<div><strong>Title:</strong> " . $key->title . "</div>";
+        echo "<div><strong>Description:</strong> " . $key->description . "</div>";
+        echo "<div><a href='code.php?id=$key->id'>Details</a></div>";
     }
 }
 
 ?>
     <!-- Navbar -->
+    <div id="overview-container">
+  <nav class="navbar">
     <?php include "../include/navbar.php" ?>
+  </nav>
 
-    <!-- Main Content -->
+  <div id="overview-content" class="content">
     <h1>Dit is overview</h1>
-    <?php  echoCode($code_array);  ?>
 
-    <!-- Footer -->
+    <?php foreach ($code_array as $key) { ?>
+      <h1><a href="code.php?id=<?php echo $key->id ?>"><?php echo $key->title ?></a></h1>
+      <p><?php echo $key->description ?></p>
+    <?php } ?>
+  </div>
+
+  <footer class="footer">
     <?php include "../include/footer.php" ?>
+  </footer>
+</div>
 
 </body>
 
