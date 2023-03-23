9f05be7b9e5f724140d1b0bb57dbac0913eb389e
139388-Joris-Taam <116798965+139388-Joris-Taam@users.noreply.github.com>
2023-03-23 09:05:35 +0100

    seach page update

--- include/search.php
+++ include/search.php
@@ -1,39 +1,32 @@
 <?php
-// Define the database connection parameters
-$host = "localhost";
-$username = "root";
-$password = "";
-$dbname = "codeexpress";
+include "connection.php";
 
-// Create a database connection
-$conn = mysqli_connect($host, $username, $password, $dbname);
+// Check if the search query parameter is set
+if (isset($_GET['q'])) {
+  // Get the search query from the URL parameter
+  $search_query = $_GET['q'];
 
-// Check if the connection is successful
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+  // Sanitize the search query to prevent SQL injection attacks
+  $search_query = mysqli_real_escape_string($conn, $search_query);
 
-// Get the search query from the URL parameter
-$search_query = $_GET['q'];
+  // Query the database for code pages that match the search query in the title
+  $sql = "SELECT * FROM configuration WHERE title LIKE '%{$search_query}%'";
 
-// Sanitize the search query to prevent SQL injection attacks
-$search_query = mysqli_real_escape_string($conn, $search_query);
+  // Execute the query
+  $result = mysqli_query($conn, $sql);
 
-// Query the database for code pages that match the search query in the title
-$sql = "SELECT * FROM overview WHERE title LIKE '%{$search_query}%'";
-
-// Execute the query
-$result = mysqli_query($conn, $sql);
-
-// Display the search results
-if (mysqli_num_rows($result) > 0) {
-  while ($row = mysqli_fetch_assoc($result)) {
-    echo "<a href='Overview.php?code_id={$row['id']}'>{$row['title']}</a><br>";
+  // Display the search results
+  if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+      echo "<a href='Overview.php?code_id={$row['id']}'>{$row['title']}</a><br>";
+    }
+  } else {
+    echo "No results found.";
   }
-} else {
-  echo "No results found.";
-}
 
-// Close the database connection
-mysqli_close($conn);
+  // Close the database connection
+  mysqli_close($conn);
+} else {
+  echo "Please enter a search query.";
+}
 ?>
