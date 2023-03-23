ce02a9f54dc7e70326ab100555eafe6b06acc0e7
Michel-3 <169541@student.horizoncollege.nl>
2023-03-23 08:26:12 +0100

    Index.php
    Removed useless tbody

--- page/index.php
+++ page/index.php
@@ -155,8 +155,6 @@ include "../include/insert-post.php";
             <input type="submit" id="submit-public" name="submit-public" href="code.php" value="Create Public post"> <input type="submit" id="submit-private" name="submit-private" value="Create Private post">
         </div>
         </form>
-        <tbody>
-        </tbody>
         </table>
         <!-- Footer -->
         <div class="item7">
