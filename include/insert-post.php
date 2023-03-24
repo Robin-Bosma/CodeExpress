9ce6efff362ef53e965d72a46c0a95904da789f0
Michel-3 <169541@student.horizoncollege.nl>
2023-03-24 12:16:16 +0100

    insert-post
    Changed the echo in to a javascript alert

--- include/insert-post.php
+++ include/insert-post.php
@@ -50,7 +50,8 @@
 
       // Execute the statement
       if ($stmt->execute()) {
-        echo "New record created successfully";
+        // Show a JavaScript alert with the success message
+        echo '<script>alert("New record created successfully");</script>';
       } else {
         $error = $stmt->errorInfo()[2];
         echo "Error: " . $error;
@@ -122,6 +123,7 @@
           echo "ERROR";
         }
 
+        echo "New record created successfully";
       } else {
         $error = $stmt2->errorInfo()[2];
         echo "Error: " . $error;
